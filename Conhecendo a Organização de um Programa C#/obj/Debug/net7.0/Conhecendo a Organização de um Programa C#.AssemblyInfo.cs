26ba6a05b98bf566b37cd9d849a707da14dc002f
adriellison <adriellisonki@gmail.com>
2022-12-11 00:26:19 -0300

    .

--- Conhecendo a Organização de um Programa C#/obj/Debug/net7.0/Conhecendo a Organização de um Programa C#.AssemblyInfo.cs
+++ Conhecendo a Organização de um Programa C#/obj/Debug/net7.0/Conhecendo a Organização de um Programa C#.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Project01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Conhecendo a Organização de um Programa C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Project01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Project01")]
+[assembly: System.Reflection.AssemblyProductAttribute("Conhecendo a Organização de um Programa C#")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Conhecendo a Organização de um Programa C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
