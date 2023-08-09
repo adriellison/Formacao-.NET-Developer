482494a4993e5dd5a82f03cf5b31ff92129b1794
Adriellison Ferreira <adriellison.ferreira@fitbank.com.br>
2023-08-09 10:51:27 -0300

    Operadores condicionais

---
+++ Fundamentos .NET/Tipos de Operadores em C#/Operadores condicionais/Program.cs
@@ -0,0 +1,29 @@
+﻿public partial class Program
+{
+    public static void Main()
+    {
+        int estoque = 10;
+        int venda = 9;
+        
+        bool possivelVender = PossivelVender(estoque, venda);
+
+        RetornoDoSistema(estoque, venda);
+    }
+
+    public static bool PossivelVender(int estoque, int venda)
+    {
+        if(venda <= 0)
+            throw new Exception("Não foi informado uma quantidade valida para venda!");
+
+        if(venda <= estoque)
+            return true;
+
+        return false;
+    }
+
+    public static void RetornoDoSistema(int quantidadeEmEstoque, int quantidadeParaVenda)
+    {
+        Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+        Console.WriteLine($"Quantidade para venda: {quantidadeParaVenda}");
+    }
+}
