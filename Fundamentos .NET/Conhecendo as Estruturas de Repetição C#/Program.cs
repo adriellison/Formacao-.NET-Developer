b1725acd8ffb9df4485040a5cdc9831cebb342bb
Adriellison Ferreira <50798465+adriellison@users.noreply.github.com>
2023-08-14 12:05:36 +0000

    Iniciando módulo de estruturas de repetição em c#

---
+++ Fundamentos .NET/Conhecendo as Estruturas de Repetição C#/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int numero = 10;
+
+        for(int contador = 0; contador <= 10; contador++)
+            Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    }
+}
---
+++ Fundamentos .NET/Operadores Aritméticos em C#/Program.cs
@@ -0,0 +1,11 @@
+﻿using Operadores_Aritméticos;
+
+public class Program
+{
+    public static void Main()
+    {
+        Calculadora calc = new Calculadora();
+
+        calc.Somar(10, 20);
+    }
+}
