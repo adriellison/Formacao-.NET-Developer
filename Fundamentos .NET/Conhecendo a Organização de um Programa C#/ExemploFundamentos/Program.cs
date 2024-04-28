668a84cec2e4ce53b8946be9ec47f7d5d609f308
Adriellison Ferreira <50798465+adriellison@users.noreply.github.com>
2024-04-28 23:15:26 +0000

    fix: Removido todos os aquivos para subir a nova solution com pastas e arquivos reestruturados para melhor entendido do projeto.

--- Fundamentos .NET/Conhecendo a Organização de um Programa C#/ExemploFundamentos/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ExemploFundamentos.Common.Models;
-
-Console.WriteLine("Hello, World!");
-
-Pessoa pessoa = new Pessoa();
-
-pessoa.Apresentar;
--- Fundamentos .NET/Sintaxe e Tipos de Dados em C#/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using SintaxeETipos.Models;
-
-Pessoa pessoa1 = new Pessoa();
-
-pessoa1.Nome = "Ana";
-pessoa1.Idade = 23;
-pessoa1.Apresentar();
