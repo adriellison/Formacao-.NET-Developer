6547bab513da92ad6fa63864959f9f924d7a864b
Adriellison Ferreira <50798465+adriellison@users.noreply.github.com>
2025-11-17 22:03:16 +0000

    feat: Adicionada implementação do método Lists para manipulação de listas

--- Fundamentos .NET/7 - Array e Listas em C#/Program.cs
+++ Fundamentos .NET/7 - Array e Listas em C#/Program.cs
@@ -18,4 +18,37 @@
             Console.WriteLine(valor);
         }
     }
+
+    public static void Lists()
+    {
+        List<string> listaString = new List<string>();
+
+        listaString = ["SP", "BA", "MG"];
+
+        listaString.Add("RJ");
+        listaString.Add("SC");
+        listaString.Add("PR");
+        listaString.Add("RS");
+
+        for (int contador = 0; contador < listaString.Count; contador++)
+        {
+            Console.WriteLine($"Posição N* {contador} - {listaString[contador]}");
+        }
+
+        foreach (string estado in listaString)
+        {
+            Console.WriteLine(estado);
+        }
+
+        listaString.Remove("MG");
+        Console.WriteLine("Após a remoção:");
+
+        foreach (string estado in listaString)
+        {
+            Console.WriteLine(estado);
+        }
+
+        Console.WriteLine($"Total de elementos na lista: {listaString.Count}");
+        Console.WriteLine($"A lista contém o estado SC? {listaString.Contains("SC")}");
+    }
 }
